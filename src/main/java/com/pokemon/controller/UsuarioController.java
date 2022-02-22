3c53d17c21f17992d6ab7eb1f363dbd8b0239f70
Chava <salvadoresai00@gmail.com>
2022-02-22 16:23:31 -0600

    Corps
    Maybe a solution for the corps for front end

--- src/main/java/com/pokemon/controller/UsuarioController.java
+++ src/main/java/com/pokemon/controller/UsuarioController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pokemon.entity.Usuario;
 import com.pokemon.reponse.UsuarioResponse;
 import com.pokemon.request.CreateUserRequest;
 import com.pokemon.service.UsuarioService;
+
 
 
 @RestController
@@ -22,8 +24,8 @@ public class UsuarioController {
 	@Autowired
 	UsuarioService usuarioService;
 	
+	@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 	@PostMapping("create")
-	
 	public UsuarioResponse createUser (@Valid @RequestBody CreateUserRequest createUserRequest) {
 		Usuario usuario = usuarioService.createUsuario(createUserRequest);
 		return new UsuarioResponse(usuario);
