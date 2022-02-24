0bb8a40736d9cb9defd12bfd2c1b0158809805b4
Chava <salvadoresai00@gmail.com>
2022-02-23 18:04:39 -0600

    Delete, update information for user, add pokemon

--- src/main/java/com/pokemon/error/GlobalExceptionHandler.java
+++ src/main/java/com/pokemon/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pokemon.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,6 +22,16 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorDetails> handleResourceNoUniqueNamesException(NoUniqueNamesException exception, 
 			WebRequest webrequest){
 		ErrorDetails error = new ErrorDetails(exception.getMessage(), webrequest.getDescription(false));
+	
+		error(error);
+		
+		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(IncorrectResultSizeDataAccessException.class)
+	public ResponseEntity<ErrorDetails> handleIncorrectResultSizeDataAccessException(IncorrectResultSizeDataAccessException exception, 
+			WebRequest webrequest){
+		ErrorDetails error = new ErrorDetails("Pokemon already exist", webrequest.getDescription(false));
 	
 		error(error);
 		
