08fb18f531d94bdad17611ffb78852f0c55f1f92
graciela1599 <17170367@itculiacan.edu.mx>
2022-02-24 14:18:19 -0700

    Improving Swagger documentation

--- src/main/java/com/pokemon/config/SwaggerConfig.java
+++ src/main/java/com/pokemon/config/SwaggerConfig.java
@@ -1,12 +1,14 @@
 package com.pokemon.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 
 @Configuration
@@ -14,14 +16,28 @@ import springfox.documentation.builders.PathSelectors;
 public class SwaggerConfig {
 
 	@Bean
-	public Docket productApi() {
+	public Docket customImplementation() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.pokemon"))
-				.paths(PathSelectors.regex("/pokemon.*"))
-				.build();
+				.apis(RequestHandlerSelectors.any())
+				
+				.paths(PathSelectors.any())
+				
+				.build()
+				.apiInfo(apiInfo());
 		
 	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title(" Pokemon Api").
+				description(" JAVA + ANGULAR Integration "
+						+ "\n API REST to register, sign in users, add pokemon to the Pokemon Team, etc."
+						+ "\n Also is a complementation of Pokedex aplication.").build();
+	}
+	
+	
 	
 
 }
